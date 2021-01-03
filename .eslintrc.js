5a4dcd9cef4c43b6cad010de224774fab25d4f47
ajay <ajaypathakcse2@gmail.com>
2021-01-03 12:46:53 +0530

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "plugin:react/recommended",
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    "no-use-before-define": "off",
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-  },
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
