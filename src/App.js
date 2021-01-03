3d014589be364afc65c3e04f37565583a33a9933
bishtk435 <kamal.bisht@daffodilsw.com>
2021-01-03 21:41:44 +0530

    added generic button, input and textarea component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Button from './components/Button'
+import InputField from './components/InputField'
+import TextArea from './components/TextArea'
 function App() {
+  const buttonClicked = () => {
+    alert('params passed!');
+  }
+
+  const buttonStyle = {
+    backgroundColor: 'red'
+  }
+
+  const inputObject = [
+    {type: 'input', label: 'First Name', placeholder: 'Type your first name'},
+    {type: 'input', label: 'Last Name', placeholder: 'Type your last name'},
+    {type: 'input', label: 'Age', placeholder: 'Type your age'},
+    {type: 'date', placeholder: 'date'},
+  ]
   return (
     <div className="App">
-     
+      {/* <InputField/> */}
+
+      {inputObject.map((item, key) => (
+        <InputField key= {key} type={item.type} label={item.label} placeholder={item.placeholder}/>
+      ))}
+      <TextArea/>
+      <Button onClick={buttonClicked}/>
     </div>
   );
 }
---
+++ src/components/InputField/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function InputField(props) {
+  // const types=['input','text','checkbox','select',]
+
+  let {
+    type = "input",
+    label = "Label",
+    placeholder = "Placeholder",
+    required = false,
+    inputStyle,
+    containerStyle,
+  } = props || {};
+
+  inputStyle = inputStyle || {
+    paddingBottom: 2,
+    padding: 8,
+    outline:'none',
+    borderColor:'gray',
+    borderWidth:1,
+    borderRadius:5,
+    width: "100%",
+    fontSize:14,
+  };
+  containerStyle = containerStyle || {
+    //  border: "1px solid black",
+    // padding: 10,
+    width: 300,
+  };
+
+  function changeBackgroundOnEnter(e) {
+    e.target.style.borderColor = "rgb(26, 172, 201)";
+  }
+  function changeBackgroundOnLeave(e) {
+    e.target.style.borderColor = 'gray';
+  }
+
+  return (
+    <div style={containerStyle}>
+      <input
+        type={type}
+        placeholder={placeholder}
+        style={inputStyle}
+        onMouseEnter={changeBackgroundOnEnter}
+        onMouseLeave={changeBackgroundOnLeave}
+      />
+    </div>
+  );
+}
